43dce23b1252c5492fb635a2b046e62c623e5d8e
REN <bunchou.rakuchan@gmail.com>
2021-10-30 21:14:05 +0900

    フィールド自由に歩けるようにした

--- tower.py
+++ tower.py
@@ -18,8 +18,13 @@ class Player:
         #プレイヤーの描画タイル 上32,33 下0,1 左34,35 右2,3 
         self.image = 32
 
-        #プレイヤーが移動中かどうか
-        self.is_moving = False
+        #プレイヤーが現在いるミニマップの座標(16の倍数のやつ)
+        self.minimap_x = 0
+        self.minimap_y = 0
+
+        #プレイヤーが何枚目のマップにいるか
+        self.map_count_x = 1
+        self.map_count_y = 1
 
 class App:
 
@@ -46,28 +51,37 @@ class App:
         pyxel.cls(0)
         self.tilemap_draw()
         self.player_draw()
+        
+        #デバッグ
+        map_x = self.player.dot_x/8
+        map_y = self.player.dot_y/8
     def tilemap_draw(self):
         base_x = 0
         base_y = 0
         tm = 0
-        u = 0
-        v = 0
+        u = self.player.minimap_x
+        v = self.player.minimap_y
         w = 16
         h = 16
 
         pyxel.bltm(base_x,base_y,tm,u,v,w,h)
+      
 
     def player_move(self):
-
-        map_x = self.player.dot_x/8
-        map_y = self.player.dot_y/8
+        x = 16*(self.player.map_count_x -1)
+        y = 16*(self.player.map_count_y -1)
+        map_x = self.player.dot_x/8 + x
+        map_y = self.player.dot_y/8 + y
         #上に移動
         if pyxel.tilemap(0).get(map_x, map_y-1) == 36:
             if pyxel.btn(pyxel.KEY_UP):
                 if pyxel.frame_count % 3 == 0:
                     self.player.dot_y = self.player.dot_y - 8
                     self.player.vectol = 0
-            
+                if  (self.player.dot_y - 8) < -8:
+                    self.player.minimap_y -= 16
+                    self.player.map_count_y -= 1
+                    self.player_udate(self.player.dot_x, 120)
         
         #下に移動
         if pyxel.tilemap(0).get(map_x, map_y+1) == 36:
@@ -75,6 +89,10 @@ class App:
                 if pyxel.frame_count % 3 == 0:
                     self.player.dot_y = self.player.dot_y + 8
                     self.player.vectol = 1
+                if  (self.player.dot_y + 8) > 128:
+                    self.player.minimap_y += 16
+                    self.player.map_count_y += 1
+                    self.player_udate(self.player.dot_x, 8)
             
         #左に移動
         if pyxel.tilemap(0).get(map_x-1, map_y) == 36:
@@ -82,12 +100,28 @@ class App:
                 if pyxel.frame_count % 3 == 0:
                     self.player.dot_x = self.player.dot_x - 8
                     self.player.vectol = 2
+
+                if (self.player.dot_x - 8) < -8:
+                    self.player.minimap_x -= 16
+                    self.player.map_count_x -= 1
+                    self.player_udate(120, self.player.dot_y)
+
         #右に移動
         if pyxel.tilemap(0).get(map_x+1, map_y) == 36:
             if pyxel.btn(pyxel.KEY_RIGHT):
                 if pyxel.frame_count % 3 == 0:
                     self.player.dot_x = self.player.dot_x + 8
                     self.player.vectol = 3
+
+                if (self.player.dot_x + 8) > 128:
+                    self.player.minimap_x += 16
+                    self.player.map_count_x += 1
+                    self.player_udate(8, self.player.dot_y)
+        
+
+    def player_udate(self, x, y):
+        self.player.dot_x = x
+        self.player.dot_y = y
 
     def player_draw(self):
         
